dcf58bd9320e20afc3de1b1691101efcb4703087
Alexanderiq2 <112217005+Alexanderiq2@users.noreply.github.com>
2022-09-15 18:42:07 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,38 +1,77 @@
 <?php
-	if (isset($_GET['hub_mode']) && isset($_GET['hub_challenge']) && isset($_GET['hub_verify_token'])) {
-		if ($_GET['hub_verify_token'] == 'my_verify_token')
-			echo $_GET['hub_challenge'];
-	} else {
-		$feedData = file_get_contents('php://input');
-		$data = json_decode($feedData);
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-		if ($data->object == "page") {
-			$commentID = $data->entry[0]->changes[0]->value->comment_id;
-			$message = $data->entry[0]->changes[0]->value->message;
-			$verb = $data->entry[0]->changes[0]->value->verb;
-			$accessToken = "EAAGP1rxZBkGcBANwqMzWEeumbMil50Op2ZA4MaW2cy1RNs6KFY23ZAgUFZB2xH144xW6c3mdCyDcYMCzJ4KaPZCZCbZA5MZCNTj5rJRTBDTRsz1lDJt8fHSP0NyStM9iSSVJJsnyLdPtotb3EZC8uV094ZBhpfB4JzdNaNABN8BfIgcowlHQoixyyVmdCZB7E7bP1VAdtoRYef4llTLzNn1NPKa";
+// add this php file to your web server and enter the complete url in AutoResponder (e.g. https://www.example.com/api_autoresponder.php)
 
-			if ($verb == "add") {
-				if (strtolower($message) == "red")
-					$reply = "Your color is: RED!";
-				else if (strtolower($message ) == "blue")
-					$reply = "Your color is: BLUE!";
-				else
-					$reply = "You didn't choose any color!";
+// to allow only authorized requests, you need to configure your .htaccess file and set the credentials with the Basic Auth option in AutoResponder
 
-				$ch = curl_init();
-				curl_setopt($ch, CURLOPT_POST, 1);
-				curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-				curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-				curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-				curl_setopt($ch, CURLOPT_POSTFIELDS, "message=$reply&access_token=$accessToken");
-				curl_setopt($ch, CURLOPT_URL, "https://graph.facebook.com/v2.10/$commentID/private_replies");
-				curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36");
-				$response = curl_exec($ch);
-				curl_close($ch);
-			}
-		}
-	}
-
+// access a custom header added in your AutoResponder rule
+// replace XXXXXX_XXXX with the name of the header in UPPERCASE (and with '-' replaced by '_')
+$myheader = $_SERVER['HTTP_XXXXXX_XXXX'];
+  
+// get posted data
+$data = json_decode(file_get_contents("php://input"));
+  
+// make sure json data is not incomplete
+if(
+	!empty($data->query) &&
+	!empty($data->appPackageName) &&
+	!empty($data->messengerPackageName) &&
+	!empty($data->query->sender) &&
+	!empty($data->query->message)
+){
+	
+	// package name of AutoResponder to detect which AutoResponder the message comes from
+	$appPackageName = $data->appPackageName;
+	// package name of messenger to detect which messenger the message comes from
+	$messengerPackageName = $data->messengerPackageName;
+	// name/number of the message sender (like shown in the Android notification)
+	$sender = $data->query->sender;
+	// text of the incoming message
+	$message = $data->query->message;
+	// is the sender a group? true or false
+	$isGroup = $data->query->isGroup;
+	// name/number of the group participant who sent the message if it was sent in a group, otherwise empty
+	$groupParticipant = $data->query->groupParticipant;
+	// id of the AutoResponder rule which has sent the web server request
+	$ruleId = $data->query->ruleId;
+	// is this a test message from AutoResponder? true or false
+	$isTestMessage = $data->query->isTestMessage;
+	
+	
+	
+	// process messages here
+	
+	$s2= date("Y");
+	$s1=$message-$s2;
+	// set response code - 200 success
 	http_response_code(200);
+
+	// send one or multiple replies to AutoResponder
+echo json_encode(array("replies" => array(
+	array("message"=>$s1)
+)));
+	
+	// or this instead for no reply:
+	// echo json_encode(array("replies" => array()));
+}
+
+// tell the user json data is incomplete
+else{
+	
+	// set response code - 400 bad request
+	http_response_code(400);
+	
+	// send error
+	echo json_encode(array("replies" => array(
+		array("message" => "Error ❌"),
+		array("message" => "JSON data is incomplete. Was the request sent by AutoResponder?")
+	)));
+}
+
 ?>
